6893c0ebbe2324f7f0ba29cc787d16f42e105fbd
cleathart <c.leathart@hotmail.co.uk>
2018-03-06 16:01:10 +0000

    minor changes to add another peice of pipe

--- mini games/public/js/cat.js
+++
@@ -1,9 +0,0 @@
-class Cat extends PIXI.Sprite {
-    constructor(app) {
-        super(PIXI.Texture.fromImage("/images/cat.png"));
-        this.anchor = 0.5;
-        this.interactive = true;
-        this.x = app.renderer.view.width / 2;
-        this.y = app.renderer.view.height / 2;
-    }
-}
